f878a4f06a3265ab70581b66527e578c3f2ad3eb
malyszaryczlowiek <45798727+malyszaryczlowiek@users.noreply.github.com>
2023-11-08 22:04:24 +0100

    Project build fine and all main methods runs too.

---
+++ scala-2.12/src/main/scala/Main.scala
@@ -0,0 +1,9 @@
+package io.github.malyszaryczlowiek
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    println(s"RUNNING")
+  }
+
+}
---
+++ scala-2.13/src/main/scala/Main.scala
@@ -0,0 +1,10 @@
+package io.github.malyszaryczlowiek
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    println(s"RUNNING")
+  }
+
+
+}

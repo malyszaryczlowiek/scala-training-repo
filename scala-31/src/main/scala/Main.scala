f419332161f7e95baf4e275db6e70b5d7feecda0
malyszaryczlowiek <45798727+malyszaryczlowiek@users.noreply.github.com>
2023-11-12 14:53:53 +0100

    scala versions updated

---
+++ scala-31/src/main/scala/Main.scala
@@ -0,0 +1,20 @@
+package io.github.malyszaryczlowiek
+
+import i10sealedtrait.{Bar, Foo}
+
+object Main:
+
+
+  def main(args: Array[String]): Unit =
+
+    val f = Foo()
+    val b = Bar
+    println(s"$f")
+    println(s"$b")
+
+  end main
+  
+  
+
+end Main
+
---
+++ scala-31/src/main/scala/i10sealedtrait/ExternalClass.scala
@@ -0,0 +1,10 @@
+package io.github.malyszaryczlowiek
+package i10sealedtrait
+
+// cannot extend sealed trait in external file
+// code below does not work
+// class ExternalClass extends SealedTrait
+
+class ExternalClass
+
+
